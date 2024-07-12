6c4fb70c68df9dd5c6d4abbdd045dc0e061932b7
Gavin Sinclair <gsinclair@gmail.com>
2024-07-12 11:35:09 +1000

    Better test runner
    Long lines in Latex output, and shows better stacktrace at the end if
    there is an error.

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,2 +1,19 @@
+#!/bin/zsh
+
+errors() {
+  tail -40 test1.log |
+    grep \.lua: |
+    grep -v /vendor/ |
+    sed 's/^.*\/luatex\/lbt/  lbt/' |
+    sed 's/ in /\n                             in /'
+}
+
 clear
-(cd ..; make install) && lualatex -halt-on-error test1.tex
+(cd ..; make install) && max_print_line=10000 lualatex -halt-on-error test1.tex
+
+if [[ $? -eq 1 ]]; then
+  echo
+  echo '= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ='
+  echo
+  errors
+fi
