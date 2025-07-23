ed5fc181411964d1ee0b7cdba96ba7a6e9a82035
Gavin Sinclair <gsinclair@gmail.com>
2025-07-24 08:08:06 +1000

    Added 'dot' and 'ddot' to simplemath

--- lbt-7-lbt-Math.lua
+++ lbt-7-lbt-Math.lua
@@ -251,6 +251,7 @@ do
                          binom
                          triangle
                          text
+                         dot ddot
                          ]]
   local alpha = makeset[[alpha beta gamma delta epsilon zeta eta theta iota
                          kappa lambda mu nu xi omicron pi rho sigma tau
