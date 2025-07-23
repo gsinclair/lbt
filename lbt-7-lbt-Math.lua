e48de50e378c068dea0a7819dcef49894f1bfe1b
Gavin Sinclair <gsinclair@gmail.com>
2025-07-24 08:15:10 +1000

    Added 'varphi' and abbreviation 'vphi' to simplemath

--- lbt-7-lbt-Math.lua
+++ lbt-7-lbt-Math.lua
@@ -255,12 +255,13 @@ do
                          ]]
   local alpha = makeset[[alpha beta gamma delta epsilon zeta eta theta iota
                          kappa lambda mu nu xi omicron pi rho sigma tau
-                         upsilon phi chi psi omega
+                         upsilon phi chi psi omega   varphi
                          Alpha Beta Gamma Delta Epsilon Zeta Eta Theta Iota
                          Kappa Lambda Mu Nu Xi Omicron Pi Rho Sigma Tau
                          Upsilon Phi Chi Psi Omega]]
   local abbrev = makemap[[al alpha be beta ga gamma de delta ep epsilon th theta la lambda
                           Al Alpha Be Beta Ga Gamma De Delta Ep Epsilon Th Theta La Lambda
+                          vphi varphi
                           imp Rightarrow
                           ds displaystyle]]
 
